f7488847ce48dba74b1f5b490243457eef82afb7
Cristian Zayas <chriss.zayas@gmail.com>
2022-12-28 05:59:27 -0800

    Secong Vertion

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-console.log('first Version')
+const listenInput = document.querySelector("#textEncrypt");
+document.querySelector("#textEncryptHidden").style.display = "block";
+document.querySelector("#positionRelartiva").style.display = "none";
+
+document.querySelector("#btnEncrypt").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let resultText = document.querySelector("#textEncrypt").value;
+  document.querySelector("#textEncryptResult").innerHTML = resultText
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+});
+
+document.querySelector("#btnDesEncrypt").addEventListener("click", (e) => {
+  e.preventDefault();
+  let resultText = document.querySelector("#textEncrypt").value;
+  document.querySelector("#textEncryptResult").innerHTML = resultText
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+});
+
+
+document.querySelector("#bntmCopy").addEventListener("click", (e) => {
+    e.preventDefault();
+    copyText()
+  });
+
+function copyText() {
+    // Selecciona el elemento de texto
+    var text = document.querySelector("#textEncryptResult");
+  
+    // Selecciona todo el texto del elemento
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(text);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  
+    // Copia el texto seleccionado al portapapeles
+    document.execCommand("copy");
+  }
