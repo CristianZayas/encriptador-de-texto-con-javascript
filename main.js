f3b35291f14fa85e7cfca56b99a5ca21e1a5750e
Cristian Zayas <chriss.zayas@gmail.com>
2022-12-30 11:52:31 -0800

    third  Vertion

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const listenInput = document.querySelector("#textEncrypt");
-document.querySelector("#textEncryptHidden").style.display = "block";
-document.querySelector("#positionRelartiva").style.display = "none";
+document.querySelector("#textEncryptHidden").style.display = "none";
+document.querySelector("#positionRelartiva").style.display = "block";
 
 document.querySelector("#btnEncrypt").addEventListener("click", (e) => {
   e.preventDefault();
-
+  document.querySelector("#textEncryptHidden").style.display = "block";
+  document.querySelector("#positionRelartiva").style.display = "none";
   let resultText = document.querySelector("#textEncrypt").value;
+  console.log(String(document.querySelector("#textEncrypt").value).trim());
+  console.log(
+    /^([a-z])+([a-z])$/g.test(
+      String(document.querySelector("#textEncrypt").value).trim()
+    )
+  );
+  if (
+    !/^([a-z])+([a-z])$/g.test(
+      String(document.querySelector("#textEncrypt").value).trim()
+    )
+  ) {
+    document.querySelector("#textEncryptHidden").style.display = "none";
+    document.querySelector("#positionRelartiva").style.display = "block";
+    return (document.querySelector("#verify").className = "red");
+  }
   document.querySelector("#textEncryptResult").innerHTML = resultText
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
@@ -25,23 +41,22 @@ document.querySelector("#btnDesEncrypt").addEventListener("click", (e) => {
     .replace(/ufat/gi, "u");
 });
 
-
 document.querySelector("#bntmCopy").addEventListener("click", (e) => {
-    e.preventDefault();
-    copyText()
-  });
+  e.preventDefault();
+  copyText();
+});
 
 function copyText() {
-    // Selecciona el elemento de texto
-    var text = document.querySelector("#textEncryptResult");
-  
-    // Selecciona todo el texto del elemento
-    var selection = window.getSelection();
-    var range = document.createRange();
-    range.selectNodeContents(text);
-    selection.removeAllRanges();
-    selection.addRange(range);
-  
-    // Copia el texto seleccionado al portapapeles
-    document.execCommand("copy");
-  }
+  // Selecciona el elemento de texto
+  var text = document.querySelector("#textEncryptResult");
+
+  // Selecciona todo el texto del elemento
+  var selection = window.getSelection();
+  var range = document.createRange();
+  range.selectNodeContents(text);
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  // Copia el texto seleccionado al portapapeles
+  document.execCommand("copy");
+}
